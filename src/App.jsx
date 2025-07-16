94f5b543ba314ae454d39e637db3c061fb5dd2ce
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2025-07-16 11:59:17 +0100

    Square

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const board = Array(9).fill(null)
+
+const Square = ({ children, updateBoard, index }) => {
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
 
 function App() {
 
-  return <h1>Tic Tac Toe</h1>
+  return ( 
+    <main className="board">
+     <h1>Tic Tac Toe</h1> 
+     <section className="game">
+      {
+        board.map((_, index) => {
+          return(
+            <Square
+              key={index}
+              index={index}
+              
+            >
+              
+            </Square>
+          )
+        })
+      }
+     </section>
+  </main>
+) 
   
 }
 
