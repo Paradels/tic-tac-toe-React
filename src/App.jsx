72a4f22008b197476ddfb304ba7cdf7f9631a057
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2025-07-16 12:11:51 +0100

    Seleccionar turno

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,55 @@
+import { useState } from "react"
 
 const TURNS = {
   X: 'x',
   O: 'o'
 }
 
-const board = Array(9).fill(null)
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
   return (
-    <div className="square">
+    <div className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
+  const [board, setBoard] = useState(
+    Array(9).fill(null))
 
-  return ( 
+  const [turn, setTurn] = useState(TURNS.X)
+  return (
     <main className="board">
-     <h1>Tic Tac Toe</h1> 
-     <section className="game">
-      {
-        board.map((_, index) => {
-          return(
-            <Square
-              key={index}
-              index={index}
-              
-            >
-              
-            </Square>
-          )
-        })
-      }
-     </section>
-  </main>
-) 
-  
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
+
 }
 
 export default App
